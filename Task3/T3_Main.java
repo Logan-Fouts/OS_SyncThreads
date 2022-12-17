a2feee3745f7526aa931f6487c5c0361c0e7a89b
Logan-Fouts <loganflogan@hotmail.com>
2022-12-17 19:18:20 +0100

    Finished

--- Task3/T3_Main.java
+++ Task3/T3_Main.java
@@ -2,6 +2,14 @@ package Task3;
 
 import java.util.concurrent.Semaphore;
 
+/*
+ * This runs the whole message queue system. Four threads are created (3 message senders, and 1 message reciver), and are started in main.
+ * The senders try to push there message onto the queue and if they cant obtain a permit, they wait. When a letter is recieved the printer
+ * prints it and if there is no messages it waits. When it is printed it is also deleted from the queue.
+ * available- the total number of spots available in the circular message queue.
+ * messagers- the total number of messages in the circular message queue.
+ * mq- the circular message queue itself.
+ */
 public class T3_Main {
     static Semaphore available = new Semaphore(5);
     static Semaphore messages = new Semaphore(0);
@@ -25,6 +33,9 @@ public class T3_Main {
         r.join();
     }
 
+    // Trys to add a message onto the message queue. If there are no available
+    // permits, it waits until there are. When there are it adds a permit to the
+    // semaphore, messages.
     public static class Sender implements Runnable {
         char letter;
 
@@ -46,12 +57,14 @@ public class T3_Main {
         }
     }
 
+    // A circular queue of letters that allows letters to be added to the list or
+    // removed only from the front.
     public static class MessageQueue {
         char[] letters;
         int start, end;
 
         public MessageQueue() {
-            start = -0;
+            start = 0;
             end = 0;
             letters = new char[5];
         }
@@ -72,6 +85,8 @@ public class T3_Main {
         }
     }
 
+    // Trys to aquire a permit from the messages semaphore, and if it does it will
+    // print the message and then remove it.
     public static class Receive implements IMessageQueue, Runnable {
 
         @Override
@@ -83,15 +98,10 @@ public class T3_Main {
                 return false;
             }
             return true;
-        }      
+        }
 
         @Override
         public char Recv() {
-            try {
-                messages.acquire();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
             mq.delete();
             return mq.getStart();
         }
@@ -99,12 +109,12 @@ public class T3_Main {
         @Override
         public void run() {
             while (!!true) {
-                char msg = Recv();
-                Send(msg);
                 try {
+                    messages.acquire();
+                    char msg = Recv();
+                    Send(msg);
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
